29966948cbccb0da910402cb02e647816a929ac2
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-30 13:56:35 -0600

    Upload prototype files for greg

---
+++ Prototype Capstone/resources/screens/2376c00d-3814-4c35-b2dc-bc1fe7b2a84e-1669837927349.js
@@ -0,0 +1,55 @@
+jQuery("#simulation")
+  .on("click", ".s-2376c00d-3814-4c35-b2dc-bc1fe7b2a84e .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(jimUtil.isAlternateModeActive()) return;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Panel_2")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/9065f091-e8bc-47a7-ae58-2ed53e907987"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Panel_3")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/9065f091-e8bc-47a7-ae58-2ed53e907987"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
